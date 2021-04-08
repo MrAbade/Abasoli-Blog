7aed5851805786672b707fff1a6c9be301ec01f6
Vinicius Abade <vinicius@kenzie.com.br>
2021-04-08 06:06:06 -0300

    feat(#1): optional load extensions when start the app

--- abasoli_blog/app.py
+++ abasoli_blog/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 
+from abasoli_blog.ext import configuration
+
+
+def simple_app():
+    app_name = __name__.split(".")[0]
+    app = Flask(app_name)
+    configuration.init_app(app)
+    return app
+
 
 def create_app():
-    app = Flask(" ".join(__name__.split(".")[0].split("_")).title())
-
-    print(app.name)
-
+    app = simple_app()
+    configuration.load_extensions(app)
     return app
