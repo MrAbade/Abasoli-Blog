277a487fda80740b2099b58e48c25aa9eea254ac
Vinicius Abade <vinicius@kenzie.com.br>
2021-04-08 06:06:44 -0300

    feat(#1): define that database is configured before, and delete configurarions config

--- abasoli_blog/utils/path_handler.py
+++ abasoli_blog/utils/path_handler.py
@@ -1,5 +1,7 @@
 from os.path import abspath
 from os import walk
+from copy import copy
+from typing import List
 
 
 def get_extension_absolute_path(app_name: str):
@@ -12,8 +14,17 @@ def get_extension_python_path(app_name: str):
     return extensions_path_in_python
 
 
+def put_the_database_first(file_list: List[str]):
+    db_index = file_list.index("database.py")
+    element_to_replace = copy(file_list[0])
+    file_list[0] = file_list[db_index]
+    file_list[db_index] = element_to_replace
+
+
 def get_files_in_path(abs_path):
     _, _, file_list = next(walk(abs_path))
+    put_the_database_first(file_list)
+    file_list.remove("configuration.py")
     return file_list
 
 
